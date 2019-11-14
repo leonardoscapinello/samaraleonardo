7f7ae91ebdf95a7e407e9ea6e43a8c75ed433e5c
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2019-11-14 20:42:40 -0300

    fix: dashboard controller return 0 instead of null

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -9,13 +9,13 @@ class DashboardController {
     const total_result = (total_credits - total_debits).toFixed(2);
     const cc_difference = (cc_payments - cc_expenses).toFixed(2);
     return res.status(200).json({
-      credits: total_credits || 0,
-      debits: total_debits || 0,
-      difference: total_result || 0,
+      credits: parseFloat(total_credits, 2) || 0,
+      debits: parseFloat(total_debits, 2) || 0,
+      difference: parseFloat(total_result, 2) || 0,
       credit_card: {
-        expenses: cc_expenses || 0,
-        payments: cc_payments || 0,
-        difference: parseFloat(cc_difference).toFixed(2),
+        expenses: parseFloat(cc_expenses, 2) || 0,
+        payments: parseFloat(cc_payments, 2) || 0,
+        difference: parseFloat(cc_difference, 2),
       },
     });
   }

807ae2fa8f14af5fb03ca7acf940bc6cacf2fb66
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2019-11-17 22:24:06 -0300

    fix: dashboard calculate error

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: '2ABkuizPJw2A',
   database: 'samaraleonardo',
   define: {
     timestamp: true,
